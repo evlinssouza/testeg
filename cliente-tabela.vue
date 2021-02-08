614d2a36da92d9ec1633e80824764de4c92a88f3
evlinssouza <77853218+evlinssouza@users.noreply.github.com>
2021-02-08 17:21:57 -0300

    Add files via upload

---
+++ cliente-tabela.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="cliente">
+      <h4>nome: {{nome}}</h4>
+      <hr>
+      <p>numero: {{numero}}</p>
+      <p>email:{{email}}</p>
+      <p>senha:{{senha}}</p>
+      <p>cpf:{{cpf}}</p>
+      <p>cnpj:{{cnpj}}</p>
+    </div>
+</template>
+    <script>
+export default {
+    data(){
+        return{
+            numero:"2323",
+            senha:"234234",
+            cpf: 0,
+            cnpj:0,
+        }
+    },
+           props: {
+            nome: String,
+            email:String,
+            idade:String,
+
+       }
+}
+</script>
+
+<style scoped>
+    #cliente{
+        background-color :pink;
+        max-width: 200px;
+        height: 150px;
+        padding: 1%;
+    }
+</style>
+
