b4ec58ccf26c6c2512c1a0639a89301fd8d0cc51
evlinssouza <77853218+evlinssouza@users.noreply.github.com>
2021-02-08 17:23:53 -0300

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h3>cadastro</h3>
+    <hr>
+</template>
+<h2>
+<script
+import cliente from './components/cliente';
+//import cadastro from './components/cadastro';
+export default {
+  name: 'App',
+  components: {
+    cliente,
+   //cadastro
+  }
+}
+</script>
+
+<style>
+</style>
